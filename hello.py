7ac8c6bb0503abe863957b60d48ad0dcb8c17826
André Rufino Papaleo <a.papaleo@aluno.ifsp.edu.br>
2026-08-24 18:42:45 +0000

    Semana 06: Adiciona formularios com Flask-WTF e sessions

--- hello.py
+++ hello.py
@@ -1,30 +1,58 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect, url_for
 from flask_moment import Moment
 from datetime import datetime, timezone
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, SelectField, PasswordField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
-# Inicializa a extensão Moment
+app.config['SECRET_KEY'] = 'chave_secreta_ifsp_dswa5'
 moment = Moment(app)
 
-# 1. Rota Home (Aula 040)
-@app.route('/')
+class HomeForm(FlaskForm):
+    nome = StringField('Informe o seu nome:', validators=[DataRequired()])
+    sobrenome = StringField('Informe o seu sobrenome:', validators=[DataRequired()])
+    instituicao = StringField('Informe a sua Insituição de ensino:', validators=[DataRequired()])
+    disciplina = SelectField('Informe a sua disciplina:', choices=[('DSWA5', 'DSWA5'), ('DWBA4', 'DWBA4'), ('Gestão de projetos', 'Gestão de projetos')])
+    submit = SubmitField('Submit')
+
+class LoginForm(FlaskForm):
+    usuario = StringField('Usuário ou e-mail', validators=[DataRequired()])
+    senha = PasswordField('Informe a sua senha', validators=[DataRequired()])
+    submit = SubmitField('Enviar')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    # Captura tempo exato
-    agora = datetime.now(timezone.utc)
-    # Envia a variável 'agora' para ser exibida no HTML
-    return render_template('index.html', current_time=agora)
-
-# 2. Rota de Identificação
-@app.route('/identificacao')
-def identificacao():
-    return render_template('identificacao.html')
-
-# 3. Rota do Contexto da Requisição
-@app.route('/contextorequisicao')
-def contexto():
-    # Captura os dados do visitante
-    user_agent = request.headers.get('User-Agent')
+    form = HomeForm()
+    
+    if form.validate_on_submit():
+        session['nome'] = form.nome.data
+        session['sobrenome'] = form.sobrenome.data
+        session['instituicao'] = form.instituicao.data
+        session['disciplina'] = form.disciplina.data
+        return redirect(url_for('index'))
+    
     ip_remoto = request.remote_addr
     host_app = request.host
-    # Envia os 3 dados capturados para o HTML
-    return render_template('contexto.html', user_agent=user_agent, ip_remoto=ip_remoto, host_app=host_app)
+    agora = datetime.now(timezone.utc)
+    
+    return render_template('index.html', form=form, 
+                           nome=session.get('nome'), 
+                           sobrenome=session.get('sobrenome'), 
+                           instituicao=session.get('instituicao'), 
+                           disciplina=session.get('disciplina'),
+                           ip_remoto=ip_remoto, 
+                           host_app=host_app,
+                           current_time=agora)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        # Agora ele salva o usuário que foi digitado na caixinha
+        session['usuario_logado'] = form.usuario.data
+        return redirect(url_for('login'))
+        
+    agora = datetime.now(timezone.utc)
+    # Envia a informação do usuário logado para o HTML
+    return render_template('login.html', form=form, usuario=session.get('usuario_logado'), current_time=agora)
